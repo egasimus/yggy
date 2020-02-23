633aede250fcab3828692a8b0ebe70db0b3c6a1a
Adam Avramov <my.new@email.here>
2020-02-23 16:06:32 +0100

    implement types, writing

--- treeProxy/traits/backend/fsSync/handles.js
+++ treeProxy/traits/backend/fsSync/handles.js
@@ -3,36 +3,75 @@ module.exports = {
   file: FileHandle
 }
 
-function BaseHandle (root, path) {
-  return {
+function BaseHandle (root, pathFragments) {
+  const self = {
+    is: {
+      mime (type) { return self.mime === type },
+      get file () { return self.stat().isFile() },
+      get dir  () { return self.stat().isDir()  },
+      get link () { return self.stat().isSymbolicLink() },
+    },
+    as: {
+      get file () { return FileHandle(root, pathFragments) },
+      get dir  () { return DirectoryHandle(root, pathFragments) }
+    },
+    get mime () {
+      return require('mime-types').lookup(this.fullPath)
+    },
     get path () {
       const {sep} = require('path')
-      return path.join(sep).replace(/\/+/, '/')
+      return pathFragments.join(sep).replace(/\/+/, '/')
+    },
+    get fullPath () {
+      const {join} = require('path')
+      return join(root, this.path)
     },
     exists () {
     },
     stat () {
+      const {statSync} = require('fs')
+      return statSync(this.fullPath)
     },
     remove () {
     },
     linkAt (...fragments) {
       const {sep, join, dirname} = require('path')
-      const {symlinkSync} = require('fs')
-      const target   = join(root, this.path)
+      const {symlinkSync, statSync, readlinkSync} = require('fs')
+      const target   = this.fullPath
       const linkPath = join(root, fragments.join(sep))
       require('mkdirp').sync(dirname(linkPath))
-      symlinkSync(target, linkPath)
+      try {
+        symlinkSync(target, linkPath)
+      } catch (error) {
+        if (error.code === 'EEXIST') {
+          try {
+            if (
+              statSync(target).isSymbolicLink() &&
+              target === readlinkSync(linkPath)
+            ) return
+          } catch (e) {
+            throw e
+          }
+        } else {
+          throw e
+        }
+      }
     },
     readlink () {
       const {join, relative} = require('path')
       const {readlinkSync} = require('fs')
       return relative(root, readlinkSync(join(root, this.path)))
-    }
+    },
   }
+  return self
 }
 
-function DirectoryHandle (root, path) {
-  return Object.assign(BaseHandle(root, path), {
+function DirectoryHandle (root, pathFragments) {
+  return Object.assign(BaseHandle(root, pathFragments), {
+    as: {
+      get file () { return FileHandle(root, pathFragments) },
+      get dir  () { return this }
+    },
     type: require('../../base/symbols').Directory,
     read   () {},
     file   () {},
@@ -41,12 +80,20 @@ function DirectoryHandle (root, path) {
   })
 }
 
-function FileHandle (root, path) {
-  return Object.assign(BaseHandle(root, path), {
+function FileHandle (root, pathFragments) {
+  return Object.assign(BaseHandle(root, pathFragments), {
+    as: {
+      get file () { return this },
+      get dir  () { return DirectoryHandle(root, pathFragmentsFragments) }
+    },
     type: require('../../base/symbols').File,
     read   () {},
-    write  () {},
-    append () {},
+    write  (data, options) {
+      require('fs').writeFileSync(this.fullPath, data, options)
+    },
+    append (data, options) {
+      require('fs').appendFileSync(this.fullPath, data, options)
+    },
     watch  () {},
   })
 }
